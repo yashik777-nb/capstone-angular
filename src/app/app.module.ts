d02b2955cd19a69a31749ad3d1eb7b7f94ebc4d9
Yash <yaswanth.iddibani@gmail.com>
2021-06-21 21:34:00 +0100

    Before Store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { IssuesComponent } from './issues/issues.component';
 import { IssuesService } from './services/issues.service';
-import { HttpClientModule } from '@angular/common/http';
+
 import { ChartsComponent } from './issues/charts/pie-chart/charts.component';
 import { ChartsModule } from 'ng2-charts';
 import { AboutComponent } from './about/about.component';
 import { SignInComponent } from './profile/sign-in/sign-in.component';
 import { RegistrationComponent } from './profile/registration/registration.component';
 import { CustomizeComponent } from './issues/customize/customize.component';
-import { FormsModule } from '@angular/forms';
+
 import { AddEditIssueComponent } from './issues/add-edit-issue/add-edit-issue.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
 import { DoughnutChartComponent } from './issues/charts/doughnut-chart/doughnut-chart.component';
-
-import { DpDatePickerModule } from 'ng2-date-picker';
 
 @NgModule({
   declarations: [
@@ -44,8 +44,6 @@ import { DpDatePickerModule } from 'ng2-date-picker';
     ChartsModule,
     FontAwesomeModule,
     FormsModule,
-    DpDatePickerModule,
-    // MyDatePickerModule,
   ],
   providers: [IssuesService],
   bootstrap: [AppComponent],
--- src/app/issues/add-edit-issue/add-edit-issue.component.ts
+++ src/app/issues/add-edit-issue/add-edit-issue.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { faSignInAlt, faIdCard } from '@fortawesome/free-solid-svg-icons';
 
-import { DatePickerComponent } from 'ng2-date-picker';
-
 @Component({
   selector: 'app-add-edit-issue',
   templateUrl: './add-edit-issue.component.html',
@@ -15,16 +13,7 @@ export class AddEditIssueComponent implements OnInit {
   severityData: string[] = ['Critical', 'Major', 'Minor'];
   statusData: string[] = ['Open', 'In Progress', 'Closed'];
 
-  @ViewChild('dayPicker') datePicker: DatePickerComponent;
-
   constructor() {}
 
   ngOnInit(): void {}
-
-  open() {
-    this.datePicker.api.open();
-  }
-  close() {
-    this.datePicker.api.close();
-  }
 }

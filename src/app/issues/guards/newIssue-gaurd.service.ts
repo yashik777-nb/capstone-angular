eacde93ad7fd652d74aface1d66ebe8bae0f43dc
Yash <yaswanth.iddibani@gmail.com>
2021-07-05 00:14:39 +0100

    Browser crash Fix

--- src/app/issues/guards/newIssue-gaurd.service.ts
+++ src/app/issues/guards/newIssue-gaurd.service.ts
@@ -2,18 +2,14 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
-  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import { Store } from '@ngrx/store';
-import * as fromApp from '../../store/app.reducer';
-
 @Injectable()
 export class AddIssueActivateGaurd implements CanActivate {
-  constructor(private route: Router, private store: Store<fromApp.AppState>) {}
+  constructor() {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -23,13 +19,8 @@ export class AddIssueActivateGaurd implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    this.store.select('userData').subscribe((storeData) => {
-      if (!storeData.authenticated) {
-        alert('Not Signed In. Please Sign In to Add Issues');
-        this.route.navigate(['sign-in']);
-      } else {
-      }
-    });
-    return false;
+    alert('Not Signed In. Please Sign In to Add Issues');
+
+    return true;
   }
 }
